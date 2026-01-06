6ece6433645049f444d8bb6be5c3bfdf067c10ad
Almaz <almazbagmanov@yandex.ru>
2026-01-06 22:03:58 +0300

    Adding resources

--- composeApp/src/commonMain/kotlin/com/bagmanov/helloworldkmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/bagmanov/helloworldkmp/App.kt
@@ -1,5 +1,6 @@
 package com.bagmanov.helloworldkmp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +9,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import helloworldkmp.composeapp.generated.resources.Res
+import helloworldkmp.composeapp.generated.resources.compose_multiplatform
+import helloworldkmp.composeapp.generated.resources.hello_world
+import helloworldkmp.composeapp.generated.resources.office_chair_svgrepo_com
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -21,7 +28,11 @@ fun App(
             contentAlignment = Alignment.Center
         ) {
             Column {
-                Text(text = "Hello, World!")
+                Image(
+                    painter = painterResource(Res.drawable.office_chair_svgrepo_com),
+                    contentDescription = null
+                )
+                Text(text = stringResource(Res.string.hello_world))
                 Text(text = "Battery level is ${batteryManager.getBatteryLevel()} %")
             }
         }
